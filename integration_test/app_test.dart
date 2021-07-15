037a9a40079539ec23498639768f8d8cb9612187
swapinal Jain <swapinalj@nurture.farm>
2021-07-15 19:56:22 +0530

    Initial commit

---
+++ integration_test/app_test.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:interactive_list/main.dart' as app;
+import 'package:interactive_list/main.dart';
+import 'package:interactive_list/static_height_scrollable/static_height_list_widget.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets("My First test", (WidgetTester tester) async {
+    app.main();
+    await tester.pumpAndSettle();
+    await tester.tap(find.byKey(Key("option_item_1")));
+    await tester.pumpAndSettle();
+    expect(find.byKey(Key("abc")), findsOneWidget);
+    expect(find.byType(HomeMenu), findsNothing);
+
+    await tester.pumpWidget(
+      StaticHeightListWidget(
+        title: 'Static Height widget',
+      ),
+    );
+    await tester.tap(find.byKey(Key("btn_next")));
+    await tester.pumpAndSettle();
+
+    expect(find.byType(StaticHeightListWidget), findsOneWidget);
+    expect(find.byType(HomeMenu), findsNothing);
+  });
+
+  testWidgets(
+    '''when clicking on next button, next image should be visible''',
+    (WidgetTester tester) async {
+      await tester.pumpWidget(
+        StaticHeightListWidget(
+          title: 'Static Height widget',
+        ),
+      );
+      await tester.pumpWidget(StaticHeightListWidget());
+      await tester.tap(find.byType(FloatingActionButton));
+      await tester.pumpAndSettle();
+
+      expect(find.byType(StaticHeightListWidget), findsOneWidget);
+      expect(find.byType(HomeMenu), findsNothing);
+    },
+  );
+}
+
+// void main() {
+//   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+//
+//   testWidgets(
+//     '''when clicking on static widget button, should go to a new page''',
+//     (WidgetTester tester) async {
+//       await tester.pumpWidget(MyApp());
+//
+//       await tester.tap(find.byKey(ValueKey("option_item_1")));
+//       await tester.pumpAndSettle();
+//
+//       expect(find.byType(StaticHeightListWidget), findsOneWidget);
+//       expect(find.byType(HomeMenu), findsNothing);
+//     },
+//   );
+//
+//   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+//
+//   testWidgets(
+//     '''when clicking on next button, next image should be visible''',
+//     (WidgetTester tester) async {
+//       await tester.pumpWidget(
+//         StaticHeightListWidget(
+//           title: 'Static Height widget',
+//         ),
+//       );
+//
+//       await tester.tap(find.byKey(ValueKey("btn_next")));
+//       await tester.pumpAndSettle();
+//
+//       expect(find.byType(StaticHeightListWidget), findsOneWidget);
+//       expect(find.byType(HomeMenu), findsNothing);
+//     },
+//   );
+// }
---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:interactive_list/scroll_positioned_list/scroll_positioned_list_widget.dart';
+import 'package:interactive_list/static_height_scrollable/static_height_list_widget.dart';
+
+import 'package:meta/meta.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeMenu(title: 'Testing Flutter 1:1'),
+    );
+  }
+}
+
+class HomeMenu extends StatelessWidget {
+  const HomeMenu({
+    Key key,
+    @required this.title,
+  }) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        key: Key('app_bar'),
+        title: Text(title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            ElevatedButton(
+              key: Key('option_item_1'),
+              onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => StaticHeightListWidget(key:ValueKey("abc"))),
+              ),
+              child: Text('Static Height Scrollable'),
+            ),
+            ElevatedButton(
+              key: Key('option_item_2'),
+              onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => ScrollPositionedListWidget()),
+              ),
+              child: Text('Dynamic Height Scrollable'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
